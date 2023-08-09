aac639fb19e2e2f2019d745e61e2205addf981df
NaashNix <thilakshanaravindu@gmail.com>
2023-08-09 20:23:16 +0530

    Menu Bar Clock created

--- src/controller/MenuBarController.java
+++ src/controller/MenuBarController.java
@@ -1,18 +1,39 @@
 package controller;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.util.Duration;
 import util.ObjectPasser;
 
 import java.net.URL;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
 public class MenuBarController implements Initializable {
 
     public Label txtUserFullName;
+    public Label lblDate;
+    public Label lblTime;
+    public LocalTime currentTime;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         txtUserFullName.setText(ObjectPasser.userFullName);
+        startClock();
     }
+
+    private void startClock() {
+        Timeline timeline = new Timeline(new KeyFrame(Duration.ZERO, e -> {
+            currentTime = LocalTime.now();
+            lblTime.setText(currentTime.format(DateTimeFormatter.ofPattern("hh:mm:ss a")));
+        }), new KeyFrame(Duration.seconds(1)));
+
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+    }
+
 }
