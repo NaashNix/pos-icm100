5164f7bd26e72a1a12e0997901fec28fad8656e2
NaashNix <thilakshanaravindu@gmail.com>
2023-08-09 21:32:09 +0530

    Item Entity created

---
+++ src/entity/Item.java
@@ -0,0 +1,20 @@
+package entity;
+
+import lombok.*;
+
+import java.sql.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Item {
+    private String itemID;
+    private String itemName;
+    private String batchNumber;
+    private double price;
+    private double qty;
+    private String supplier;
+    private Date expireDate;
+}
